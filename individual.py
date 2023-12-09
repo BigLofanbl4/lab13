918734ce0b76c7c484482ca4714778a19c6830e8
BigLofanbl4 <smallofanbl4@gmail.com>
2023-12-09 13:53:40 +0300

    refactor

--- individual.py
+++ individual.py
@@ -4,7 +4,8 @@
 
 def prod(*nums):
     if nums:
-        max_elem_index = [abs(i) for i in nums].index(max([abs(i) for i in nums]))
+        abs_nums = [abs(i) for i in nums]
+        max_elem_index = abs_nums.index(max(abs_nums))
         res = 1
 
         for i in nums[max_elem_index + 1 :]:
